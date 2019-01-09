e66f7de12353fe54f49fb9e10ab36a06f7aaba77
dapengchina <aoqishequ@163.com>
2019-01-09 09:41:25 +0800

    Change-Id: I0f17eaa9a59ba2159c1da991557d81b080b630dd

--- src/main/java/com/csi/sbs/deposit/business/controller/CustomerMasterController.java
+++ src/main/java/com/csi/sbs/deposit/business/controller/CustomerMasterController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.csi.sbs.common.business.httpclient.ConnPostClient;
 import com.csi.sbs.deposit.business.clientmodel.CustomerAndAccountModel;
 import com.csi.sbs.deposit.business.service.CustomerMasterService;
 import com.csi.sbs.deposit.business.util.UUIDUtil;
@@ -39,8 +40,18 @@ public class CustomerMasterController {
 
            Map<String,Object> map = new HashMap<String,Object>();
            try{
-        	   cam.getCustomer().setClearingcode("001");
-               cam.getCustomer().setBranchnumber("0003");
+        	   Map<String,String> param1 = new HashMap<String,String>();
+        	   
+        	   
+        	   param1.put("item", "Clearing_Code,Branch_Number");
+        	   @SuppressWarnings("unused")
+        	   //String temp = "{\"item\":\"Clearing_Code,Branch_Number\"}";
+        	   //ConnPostClient
+        	   //ConnPostClient.postMap(url, map);
+        	   //String str = ConnPostClient.postJson("http://localhost:8083/sysconfig/query", temp);
+			   String str = ConnPostClient.postMap("http://localhost:8083/sysconfig/query", param1);
+//        	   cam.getCustomer().setClearingcode("001");
+//               cam.getCustomer().setBranchnumber("0003");
                cam.getCustomer().setCustomernumber("0010003");
                cam.getCustomer().setCustomerid(UUIDUtil.generateUUID());
                cam.getCustomer().setId(UUIDUtil.generateUUID());
